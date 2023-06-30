cd8c7524fd335276e46515afeeaa25a97eac5d33
Yuvraj Singh <ysrishu1031@gmail.com>
2023-06-30 18:22:44 +0530

    Fixed some bugs

--- Book Store/cmd/main/main.go
+++ Book Store/cmd/main/main.go
@@ -1,16 +1,19 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
 
 	"github.com/YuvrajSingh3110/bookstore/pkg/routes"
 	"github.com/gorilla/mux"
+	_ "github.com/jinzhu/gorm/dialects/mysql"
 )
 
 func main() {
+	fmt.Println("Welcome to Bookstore...")
 	r := mux.NewRouter()
 	routes.RegisterBookStoreRoutes(r)
 	http.Handle("/", r)
-	log.Fatal(http.ListenAndServe("localhost:3000", r))
+	log.Fatal(http.ListenAndServe("localhost:8000", r))
 }
--- Book Store/pkg/config/app.go
+++ Book Store/pkg/config/app.go
@@ -1,6 +1,8 @@
 package config
 
 import (
+	"fmt"
+
 	"github.com/jinzhu/gorm"
 	_ "github.com/jinzhu/gorm/dialects/mysql"
 )
@@ -10,10 +12,11 @@ var (
 )
 
 func Connect() {
-	d, err := gorm.Open("mysql", "Yuvraj:Yato@/bookstore?charset=utf8&parseTime=True&loc=Local")
+	d, err := gorm.Open("mysql", "root:Yuvraj@3110@/bookstore?charset=utf8&parseTime=True&loc=Local")
 	if err != nil {
 		panic(err)
 	}
+	fmt.Println("Conected to my sql successfully")
 	db = d
 }
 
