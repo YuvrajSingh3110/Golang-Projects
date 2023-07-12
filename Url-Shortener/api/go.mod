b0a042847da74870dc03f893b339f1a721d0bf0b
Yuvraj Singh <ysrishu1031@gmail.com>
2023-07-12 08:46:12 +0530

    completed resolve and database files

--- Url-Shortener/api/go.mod
+++ Url-Shortener/api/go.mod
@@ -2,8 +2,12 @@ module github.com/YuvrajSingh3110/Url_Shortener
 
 go 1.20
 
+require github.com/go-redis/redis/v8 v8.11.5
+
 require (
 	github.com/andybalholm/brotli v1.0.5 // indirect
+	github.com/cespare/xxhash/v2 v2.1.2 // indirect
+	github.com/dgryski/go-rendezvous v0.0.0-20200823014737-9f7001d12a5f // indirect
 	github.com/gofiber/fiber/v2 v2.47.0 // indirect
 	github.com/google/uuid v1.3.0 // indirect
 	github.com/joho/godotenv v1.5.1 // indirect
--- Url-Shortener/api/routes/shorten.go
+++ Url-Shortener/api/routes/shorten.go
@@ -1,6 +1,11 @@
 package routes
 
-import "time"
+import (
+	"time"
+
+	"github.com/YuvrajSingh3110/Url_Shortener/helpers"
+	"github.com/gofiber/fiber/v2"
+)
 
 type Request struct {
 	Url         string        `json:"url"`
@@ -14,4 +19,27 @@ type Response struct {
 	Expiry         time.Duration `json:"expiry"`
 	XRateRemaining int           `json:"rate_limit"`
 	XRateReset     time.Duration `json:"rate_limit_reset"`
+}
+
+func ShortenUrl(c *fiber.Ctx) error {
+	body := new(Request)
+	if err := c.BodyParser(&body); err != nil{
+		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{"error":"cannot parse JSON"})
+	}
+
+	//rate limiting
+
+	//check if input is an url
+	if !govalidator.IsURL(body.Url){
+		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{"error":"invalid URL"})
+	}
+
+	//checking for domain error to prevent infinite loop
+	if !helpers.RemoveDomainError(body.Url){
+		return c.Status(fiber.StatusServiceUnavailable).JSON(fiber.Map{"error":"you cannot hack this system :)"})
+	}
+
+	//enforce https, SSL
+	body.Url = helpers.EnforceHTTP(body.Url)
+	return nil
 }
