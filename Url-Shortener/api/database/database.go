dca3e603d791c4dfad8c8d9d9aa0dab904d3e912
Yuvraj Singh <ysrishu1031@gmail.com>
2023-07-12 14:58:35 +0530

    shorten func, dockerfile and docker-compose file

--- Url-Shortener/api/database/database.go
+++ Url-Shortener/api/database/database.go
@@ -10,11 +10,12 @@ import (
 var Ctx = context.Background()
 
 func CreateClient(dbNo int) *redis.Client {
+	//redis is based on key-value pair
 	rdb := redis.NewClient(&redis.Options{
 		Addr: os.Getenv("DB_ADDR"),
 		Password: os.Getenv("DB_PASS"),
 		DB: dbNo,
 	})
-	
+
 	return rdb
 }
--- Url-Shortener/api/routes/shorten.go
+++ Url-Shortener/api/routes/shorten.go
@@ -1,10 +1,16 @@
 package routes
 
 import (
+	"os"
+	"strconv"
 	"time"
 
+	"github.com/YuvrajSingh3110/Url_Shortener/database"
 	"github.com/YuvrajSingh3110/Url_Shortener/helpers"
+	"github.com/asaskevich/govalidator"
+	"github.com/go-redis/redis/v8"
 	"github.com/gofiber/fiber/v2"
+	"github.com/google/uuid"
 )
 
 type Request struct {
@@ -23,23 +29,89 @@ type Response struct {
 
 func ShortenUrl(c *fiber.Ctx) error {
 	body := new(Request)
-	if err := c.BodyParser(&body); err != nil{
-		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{"error":"cannot parse JSON"})
+	if err := c.BodyParser(&body); err != nil {
+		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{"error": "cannot parse JSON"})
 	}
 
 	//rate limiting
+	r2 := database.CreateClient(1)
+	defer r2.Close()
+	//we check for ip address
+	val, err := r2.Get(database.Ctx, c.IP()).Result()
+	//if u didn't find any value in the db which means user is using the api for the 1st time
+	if err == redis.Nil {
+		_ = r2.Set(database.Ctx, c.IP(), os.Getenv("API_QUOTA"), 30*60*time.Second).Err()
+	} else { //user found
+		val, _ = r2.Get(database.Ctx, c.IP()).Result()
+		valInt, _ := strconv.Atoi(val)
+		if valInt <= 0 {
+			limit, _ := r2.TTL(database.Ctx, c.IP()).Result()
+			return c.Status(fiber.StatusServiceUnavailable).JSON(fiber.Map{
+				"error":            "rate limit exceeded",
+				"rate_limit_reset": limit / time.Nanosecond / time.Minute,
+			})
+		}
+	}
 
 	//check if input is an url
-	if !govalidator.IsURL(body.Url){
-		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{"error":"invalid URL"})
+	if !govalidator.IsURL(body.Url) {
+		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{"error": "invalid URL"})
 	}
 
 	//checking for domain error to prevent infinite loop
-	if !helpers.RemoveDomainError(body.Url){
-		return c.Status(fiber.StatusServiceUnavailable).JSON(fiber.Map{"error":"you cannot hack this system :)"})
+	if !helpers.RemoveDomainError(body.Url) {
+		return c.Status(fiber.StatusServiceUnavailable).JSON(fiber.Map{"error": "you cannot hack this system :)"})
 	}
 
 	//enforce https, SSL
 	body.Url = helpers.EnforceHTTP(body.Url)
-	return nil
+
+	var id string
+	if body.CustomShort == "" {
+		id = uuid.New().String()[:6]
+	} else {
+		id = body.CustomShort
+	}
+
+	r := database.CreateClient(0)
+	defer r.Close()
+	val, _ = r.Get(database.Ctx, id).Result()
+	if val != "" {
+		//something is found with that uid
+		return c.Status(fiber.StatusForbidden).JSON(fiber.Map{
+			"error": "url custom is already in use",
+		})
+	}
+
+	if body.Expiry == 0 {
+		body.Expiry = 24
+	}
+
+	err = r.Set(database.Ctx, body.Url, body, body.Expiry*3600*time.Second).Err()
+	if err != nil {
+		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
+			"error":"unable to connect to server",
+		})
+	}
+
+	//sending the reponse
+	resp := Response{
+		Url: body.Url,
+		CustomShort: "",
+		Expiry: body.Expiry,
+		XRateRemaining: 10,
+		XRateReset: 30,
+	}
+
+	r2.Decr(database.Ctx, c.IP())
+
+	val, _ = r2.Get(database.Ctx, c.IP()).Result()
+	resp.XRateRemaining, _ = strconv.Atoi(val)
+
+	ttl, _ := r2.TTL(database.Ctx, c.IP()).Result()
+	resp.XRateReset = ttl / time.Nanosecond / time.Minute
+	
+	resp.CustomShort = os.Getenv("DOMAIN") + "/" + id
+
+	return c.Status(fiber.StatusOK).JSON(resp)
 }
