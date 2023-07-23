0f508357cc5cded1193d270e5501c3d48febee25
Yuvraj Singh <ysrishu1031@gmail.com>
2023-07-23 15:41:45 +0530

    created models and storage

--- Go-Fiber-Postgres/main.go
+++ Go-Fiber-Postgres/main.go
@@ -2,7 +2,11 @@ package main
 
 import (
 	"log"
+	"net/http"
+	"os"
 
+	"github.com/YuvrajSingh3110/goFiberPostgres/models"
+	"github.com/YuvrajSingh3110/goFiberPostgres/storage"
 	"github.com/gofiber/fiber/v2"
 	"github.com/jinzhu/gorm"
 	"github.com/joho/godotenv"
@@ -16,6 +20,48 @@ type Book struct {
 	Author    string `json:"author"`
 	Title     string `json:"title"`
 	Publisher string `json:"publisher"`
+}
+
+func (r *Repository) CreateBook(context *fiber.Ctx) error {
+	book := Book{}
+	err := context.BodyParser(&book)
+	if err != nil {
+		context.Status(http.StatusUnprocessableEntity).JSON(&fiber.Map{
+			"message": "Request failed",
+		})
+		return err
+	}
+
+	err = r.DB.Create(&book).Error
+	if err !=nil{
+		context.Status(http.StatusBadRequest).JSON(&fiber.Map{
+			"error": "Could not create the book",
+		})
+		return err
+	}
+
+	context.Status(http.StatusOK).JSON(&fiber.Map{
+		"message": "Book has been created",
+	})
+	return nil
+}
+
+func (r *Repository) GetBooks(context *fiber.Ctx) error {
+	bookModels := &[]models.Books{}
+
+	err := r.DB.Find(bookModels).Error
+	if err !=nil{
+		context.Status(http.StatusBadRequest).JSON(&fiber.Map{
+			"error": "Could not get books",
+		})
+		return err
+	}
+
+	context.Status(http.StatusOK).JSON(&fiber.Map{
+		"message": "Books fetched successfully",
+		"data": bookModels,
+	})
+	return nil
 }
 
 func (r *Repository) SetupRoutes(app *fiber.App) {
@@ -32,6 +78,14 @@ func main() {
 		log.Fatal(err)
 	}
 
+	config := &storage.Config{
+		Host: os.Getenv("DB_HOST"),
+		Port: os.Getenv("DB_PORT"),
+		Password: os.Getenv("DB_PASSWORD"),
+		User: os.Getenv("DB_USER"),
+		SSLMode: os.Getenv("DB_SSLMODE"),
+		DBName: os.Getenv("DB_NAME"),
+	}
 	db, err := storage.NewConnection(config)
 	if err != nil {
 		log.Fatal("Could not load the database")
